4676fdfc2e4d280dba0ce9e800083961049aebfc
Michael Zhang <mail@mzhang.io>
2021-09-12 03:01:12 -0500

    auth with osu works now

---
+++ src/lib/consts.ts
@@ -0,0 +1,4 @@
+export let OSU_API_BASE = "https://osu.ppy.sh/api/v2";
+export let OSU_AUTHORIZE_ENDPOINT = "https://osu.ppy.sh/oauth/authorize";
+export let OSU_TOKEN_ENDPOINT = "https://osu.ppy.sh/oauth/token";
+
--- src/routes/auth/login.ts
+++ src/routes/auth/login.ts
@@ -1,15 +1,20 @@
-export async function get(req) {
-  let config = req.locals.config;
-  let osuAuthorizeEndpoint = "https://osu.ppy.sh/oauth/authorize";
-  let callbackUrl = `${config.publicUrl}/auth/callback`;
-  console.log("Config", config);
+import { OSU_AUTHORIZE_ENDPOINT } from "$lib/consts";
+import { osuClientId, publicUrl } from "$lib/config";
+
+export async function get(_) {
+  let callbackUrl = `${publicUrl}/auth/callback`;
+
+  let redirectUrl = new URL(OSU_AUTHORIZE_ENDPOINT);
+  redirectUrl.searchParams.append("client_id", osuClientId);
+  redirectUrl.searchParams.append("redirect_uri", callbackUrl);
+  redirectUrl.searchParams.append("response_type", "code");
 
   return {
     // redirect
     status: 302,
 
     headers: {
-      location: `${osuAuthorizeEndpoint}?client_id=${config.osuClientId}&redirect_uri=${callbackUrl}`,
+      location: redirectUrl,
     },
   };
 }
